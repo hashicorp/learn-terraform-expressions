07653e8a7023d172e2e52b562180e2c85666d90a
Rachel Sharp <rachel@hashicorp.com>
2020-10-08 17:52:00 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_elb" "learn" {
+  name               = "elb-learn"
+  availability_zones = ["us-east-1a", "us-east-2b"]
+  
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.ubuntu[*].id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "publics3" {
+  acl    = "public-read"
+  bucket = random_id.id.hex
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_instance" "ubuntu" {
+  count                       = 3
+  ami                         = "ami-2e1ef954"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.my_subnet.id
+}
