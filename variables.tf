9592a6c06b77a0e7d01f3936d9bddc7d3ddc9779
Rachel Sharp <rachel@hashicorp.com>
2020-10-18 21:19:57 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,13 +7,6 @@ variable "cidr_subnet" {
   default     = "172.16.10.0/24"
 }
 
-/*variable "instance_count" {
-  type        = bool
-  description = "If this is a multiple instance deployment, choose `true` to deploy 3 instances"
-  default     = true
-}
-*/
-
 variable "name" {
   default = "Learn"
 }
