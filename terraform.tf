824d07a1bb99d31f2b3a4408709609619bf47ac6
Brian McClain <brianmmcclain@gmail.com>
2024-04-30 10:43:51 -0400

    Bump Ubuntu version, loosen Terraform version restriction (#7)
    * Bump Ubuntu version
    
    * Bump AWS provider, loosen restriction on Terraform version

--- terraform.tf
+++ terraform.tf
@@ -14,10 +14,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.4.0"
+      version = "~> 5.47.0"
     }
   }
 
-  required_version = "~> 1.1.0"
+  required_version = "~> 1.1"
 }
 
